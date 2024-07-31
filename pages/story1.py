0335f1f9a4db697781a74f6a51f0adcdc00954ef
Chun-Jihun <wjswlgns101@gmail.com>
2024-07-31 19:16:25 +0900

    엔딩 출력

--- pages/story1.py
+++ pages/story1.py
@@ -371,7 +371,7 @@ elif st.session_state.step == 4:
         """
     )
 
-    temp_query = f"KPC는 플레이어가 스토리를 잘 진행할 수 있도록 게임 내에서 내레이터가 조종하여 이끌어주는 캐릭터로, 플레이어의 행동에 과한 개입은 하지 않는다. 또한 KPC라는 단어를 언급해서는 안되며 KPC라는 단어 대신 {st.session_state.kpc_name}으로 수정하여 출력하라. KPC에 대한 직접적인 질문에 대해서는 처음 듣는 단어처럼 행동하라. If you encounter something you don't know, guide the user to follow the provided Context. Do not create information that is not present in the Context under any circumstances. If the story is nearing its end, wrap up the narrative and print [엔딩] at the very end.\n"
+    temp_query = f"KPC는 플레이어가 스토리를 잘 진행할 수 있도록 게임 내에서 내레이터가 조종하여 이끌어주는 캐릭터로, 플레이어의 행동에 과한 개입은 하지 않는다. 또한 KPC라는 단어를 언급해서는 안되며 KPC라는 단어 대신 {st.session_state.kpc_name}으로 수정하여 출력하라. KPC에 대한 직접적인 질문에 대해서는 처음 듣는 단어처럼 행동하라. If you encounter something you don't know, guide the user to follow the provided Context. Do not create information that is not present in the Context under any circumstances. Once all the [ED] conditions are met, output the respective [ED] and conclude the story immediately.And print [엔딩] at the very end.\n"
 
     story_query = """
          PC는 플레이어가 조종하는 캐릭터로, 너가 직접 대화를 생성하거나 행동을 조종해서는 안된다. 플레이어의 이름 또는 당신으로 수정하여 출력하라. 또한 PC라는 단어를 언급해서는 안된다.
